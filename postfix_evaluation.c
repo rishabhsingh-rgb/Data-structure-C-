895147874c61ed352181967170e444da56799cb1
Rishabh Singh <rishabhsingh742006@gmail.com>
2026-04-01 21:27:14 +0530

    Implement pop function for postfix evaluation

--- postfix_evaluation.c
+++ postfix_evaluation.c
@@ -26,3 +26,59 @@ void push(int item)
     top=temp;
     stackSize++;
 }
+void pop(char sym)
+{
+    struct node *temp;
+    int pop1,pop2;
+    if(stackSize < 2)
+    {
+        printf("Error: insufficient operands for '%c'\n", sym);
+        exit(1);
+    }
+    else if(top==NULL)
+    {
+        printf("Stack underflow.\n");
+    }
+    else
+    {
+        temp=top;
+        pop1=top->value;
+        top=top->next;
+        free(temp);
+        temp=top;
+        pop2=top->value;
+        top=top->next;
+        free(temp);
+    }
+    stackSize-=2;
+    if(sym=='*')
+    {
+        push(pop2*pop1);
+    }
+    else if(sym=='/')
+    {
+        if(pop1==0)
+        {
+            printf("Error: division by zero\n");
+            exit(1);
+        }
+        push(pop2/pop1);
+    }
+    else if(sym=='+')
+    {
+        push(pop2+pop1);
+    }
+    else if(sym=='-')
+    {
+        push(pop2-pop1);
+    }
+    else if(sym=='^')
+    {
+        push(pow(pop2,pop1));
+    }
+    else
+    {
+        printf("Invalid operator %c\n", sym);
+        exit(1);
+    }
+}
